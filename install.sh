bd900b6b39d371b2b2c82fd7038b1e659382140a
Wes AuYeung <wauyeung@yipitdata.com>
2017-03-15 23:27:29 -0400

    Edited bash-powerline command in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-BASH_FILE_NAMES=(bashrc bash_profile bash_powerline pystartup xpather.py)
+BASH_FILE_NAMES=(bashrc bash_profile bash-powerline.sh pystartup xpather.py)
 
 for file_name in ${BASH_FILE_NAMES[@]}; do
 	ln -s ~/.bash-config/.${file_name} ~/.${file_name}
