725dedc8a20bb2f850c7c34d71eebd41af4f26a3
Wes AuYeung <wauyeung@yipitdata.com>
2018-08-28 10:30:26 -0400

    * Added comments to clarify installation steps * Added installation of powerline fonts

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
-TGT_FILE_NAMES=(bashrc bash_profile bash-powerline.sh pystartup xpather.py)
-
+# Install itermocil
 brew install TomAnthony/brews/itermocil
+
+# Install powerline-shell
 pip install powerline-shell
+
+mkdir -p ~/.config/powerline-shell
+ln -Fhs "$(pwd)/powerline-config.json" ~/.config/powerline-shell/config.json
+
+# Install powerline fonts & clean-up after
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Create sym links for needed files & clean-up name-space
+TGT_FILE_NAMES=(bashrc bash_profile bash-powerline.sh pystartup xpather.py)
 
 for file_name in ${TGT_FILE_NAMES[@]}; do
 	ln -Fhs $(pwd)/${file_name} ~/.${file_name}
 done
-
-mkdir -p ~/.config/powerline-shell
-ln -Fhs "$(pwd)/powerline-config.json" ~/.config/powerline-shell/config.json
 
 unset file_name TGT_FILE_NAMES
